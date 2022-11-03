e8b7fc2075b5284d1c6646de963791fbb00979b4
BamBam-sys <lawalayobamidavid@gmail.com>
2022-11-03 21:22:58 +0100

    style: refactor container class

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Footer, Links, Profile, Socials } from '../components';
 
+import '../sass/home.scss';
+
 const Home = () => {
   return (
     <>
-      <div className="container">
-        <Profile />
-        <Links />
-        <Socials />
+      <div className="home">
+        <div className="container">
+          <Profile />
+          <Links />
+          <Socials />
+        </div>
       </div>
       <Footer />
     </>
