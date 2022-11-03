8d9dfbe396be178b1a801fefc43371d6dfc6b893
BamBam-sys <lawalayobamidavid@gmail.com>
2022-11-03 23:13:58 +0100

    feat: create input component

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import Link from './Link';
 import Links from './Links';
 import Profile from './Profile';
 import Socials from './Socials';
+import Input from './Input';
 
-export { Footer, Link, Links, Profile, Socials };
+export { Footer, Link, Links, Profile, Socials, Input };
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,41 @@
 import React from 'react';
+import { Input } from '../components';
+import '../sass/contact.scss';
 
 const Contact = () => {
-  return <div className="container contact"></div>;
+  return (
+    <div className="container contact">
+      <div className="content">
+        <h2>Contact Me</h2>
+        <p>Hi there, contact me to ask me about anything you have in mind.</p>
+
+        <form>
+          <Input
+            id="first_name"
+            label="First name"
+            name="first_name"
+            placeholder="Enter your first name"
+            type="text"
+          />
+          <Input
+            id="last_name"
+            label="Last name"
+            name="last_name"
+            placeholder="Enter your last name"
+            type="text"
+          />
+          <Input
+            id="email"
+            label="Email"
+            name="email"
+            placeholder="yourname@gmail.com"
+            type="email"
+          />
+          {/* <TextArea /> */}
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
